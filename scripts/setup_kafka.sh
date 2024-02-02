c8a3f98629358366b62ee7ad6919cbaaac3ca7b7
Deepanshu Yadav <rohantheyadav@gmail.com>
2024-02-02 23:13:53 +0100

    corr
    Signed-off-by: Deepanshu Yadav <rohantheyadav@gmail.com>

--- scripts/setup_kafka.sh
+++ scripts/setup_kafka.sh
@@ -62,5 +62,5 @@ echo "Installed python requirments."
 
 echo "Running python script."
 
-cd /opt/stream_project/kafka/ && python3 producer.py && >> /opt/log.out 2>&1 &
+cd /opt/stream_project/kafka/ && sudo python3 producer.py && >> /opt/log.out 2>&1 &
 disown -h %1
